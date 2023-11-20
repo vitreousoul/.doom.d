035ab763494c878484e17fb4e2bd5eefadd657b2
Ryan Merritt <merrittaudio@gmail.com>
2023-11-19 23:14:16 -0600

    no agenda files

--- custom.el
+++ custom.el
@@ -4,9 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
- '(org-agenda-files
-   '("~/docs/school/agenda.org" "/Users/ryan/org/disassembly.org" "/Users/ryan/org/expenses.org" "/Users/ryan/org/what_kind_of_programming.org")))
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
